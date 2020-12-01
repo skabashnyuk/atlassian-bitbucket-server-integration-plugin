6fc0abdb6e110e4dc614a3afb4da8dfb9942ae3a
ali3-atlassian <74999950+ali3-atlassian@users.noreply.github.com>
2020-12-01 11:28:51 +1100

    unhid id field for personal access token (#218)
    * unhid id field for personal access token
    
    * PR changes to placeholder

--- src/main/resources/com/atlassian/bitbucket/jenkins/internal/config/BitbucketTokenCredentialsImpl/config.groovy
+++ src/main/resources/com/atlassian/bitbucket/jenkins/internal/config/BitbucketTokenCredentialsImpl/config.groovy
@@ -6,10 +6,10 @@ f.entry(title: _("bitbucket.admin.token"), field: "secret") {
     f.password(value: instance?.secret, placeholder: "This token must have project admin permissions")
 }
 
-f.entry(title: _("bitbucket.admin.token.description"), field: "description") {
-    f.textbox(placeholder: "To help administrators identify this token")
+f.entry(title: _("bitbucket.admin.token.id"), field: "id") {
+    f.textbox(value: instance?.id, placeholder: "Enter a unique ID for this token or leave blank to auto-generate")
 }
 
-f.invisibleEntry(field: "id") {
-    f.input(type: "hidden", name: "id", value: "${instance?.id}")
+f.entry(title: _("bitbucket.admin.token.description"), field: "description") {
+    f.textbox(placeholder: "To help administrators identify this token")
 }
